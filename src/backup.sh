cc7fa21c9cf7af1e7a9aaced2ae161a0d02de296
sudoshmudo <sudoshmudo>
2021-11-15 12:46:19 +0100

    using database urlž

--- src/backup.sh
+++ src/backup.sh
@@ -18,46 +18,20 @@ if [ -z "$S3_BUCKET" ]; then
   exit 1
 fi
 
-if [ -z "$POSTGRES_DATABASE" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_HOST" ]; then
-  if [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
-    POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
-    POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
-  else
-    echo "You need to set the POSTGRES_HOST environment variable."
-    exit 1
-  fi
-fi
-
-if [ -z "$POSTGRES_USER" ]; then
-  echo "You need to set the POSTGRES_USER environment variable."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_PASSWORD" ]; then
-  echo "You need to set the POSTGRES_PASSWORD environment variable."
+if [ -z "$DATABASE_URL" ]; then
+  echo "You need to set the DATABASE_URL environment variable."
   exit 1
 fi
 
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$S3_REGION
-export PGPASSWORD=$POSTGRES_PASSWORD
+export DATABASE_NAME="${DATABASE_URL##*/}"
 
-echo "Creating backup of $POSTGRES_DATABASE database..."
-pg_dump --format=custom \
-        -h $POSTGRES_HOST \
-        -p $POSTGRES_PORT \
-        -U $POSTGRES_USER \
-        -d $POSTGRES_DATABASE \
-        $PGDUMP_EXTRA_OPTS \
-        > db.dump
+echo "Creating backup of $DATABASE_NAME database..."
+pg_dump --format=custom -d $DATABASE_URL > db.dump
 
-s3_uri_base="s3://${S3_BUCKET}/${POSTGRES_DATABASE}.dump"
+s3_uri_base="s3://${S3_BUCKET}/${DATABASE_NAME}.dump"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
--- src/restore.sh
+++ src/restore.sh
@@ -18,31 +18,15 @@ if [ -z "$S3_BUCKET" ]; then
   exit 1
 fi
 
-if [ -z "$POSTGRES_DATABASE" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_HOST" ]; then
-  echo "You need to set the POSTGRES_HOST environment variable."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_USER" ]; then
-  echo "You need to set the POSTGRES_USER environment variable."
-  exit 1
-fi
-
-if [ -z "$POSTGRES_PASSWORD" ]; then
-  echo "You need to set the POSTGRES_PASSWORD environment variable" \
-       "or link to a container named POSTGRES."
+if [ -z "$DATABASE_URL" ]; then
+  echo "You need to set the DATABASE_URL environment variable."
   exit 1
 fi
 
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$S3_REGION
-export PGPASSWORD=$POSTGRES_PASSWORD
+export DATABASE_NAME="${DATABASE_URL##*/}"
 
 s3_uri_base="s3://${S3_BUCKET}"
 
@@ -53,11 +37,11 @@ else
 fi
 
 if [ $# -eq 1 ]; then
-  key_suffix="${POSTGRES_DATABASE}${file_type}"
+  key_suffix="${DATABASE_NAME}${file_type}"
 else
   echo "Finding latest backup..."
   key_suffix=$(
-    aws s3 ls "${s3_uri_base}/${POSTGRES_DATABASE}" \
+    aws s3 ls "${s3_uri_base}/${DATABASE_NAME}" \
       | sort \
       | tail -n 1 \
       | awk '{ print $4 }'
@@ -73,7 +57,7 @@ if [ -n "$PASSPHRASE" ]; then
   rm db.dump.gpg
 fi
 
-conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE"
+conn_opts="-d $DATABASE_URL"
 
 echo "Restoring from backup..."
 pg_restore $conn_opts --clean --if-exists db.dump
